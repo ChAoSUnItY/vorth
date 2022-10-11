09f8720ab66e5f5ad2bc7b77b085ac6686ff63f3
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-10-12 01:56:41 +0800

    add token transforming & fix lexing

--- src/compilation.ts
+++ src/compilation.ts
@@ -1,6 +1,8 @@
 export class Compilation {
     private _stackChain = new StackChain();
 
+    // File Lexing
+
     private findCol(line: string, start: number, predicate: (input: string) => boolean): number {
         while (start < line.length && !predicate(line[start]))
             start++;
@@ -8,28 +10,65 @@ export class Compilation {
     }
 
     private lexLine(line: string): [number, string][] {
-        let tokens: [number, string][] = [];
+        const whitespaceReg = /\s/;
+        const tokens: [number, string][] = [];
 
-        let col = this.findCol(line, 0, s => /\w/.test(s));
+        let col = this.findCol(line, 0, s => !whitespaceReg.test(s));
         while (col < line.length) {
-            let col_end = this.findCol(line, col, s => !/\w/.test(s));
+            const col_end = this.findCol(line, col, s => whitespaceReg.test(s));
             tokens.push([col, line.substring(col, col_end)]);
-            col = this.findCol(line, col_end, s => /\w/.test(s));
+            col = this.findCol(line, col_end, s => !whitespaceReg.test(s));
         }
 
         return tokens;
     }
 
+    private lexFile(fileName: string, source: string): [string, number, number, string][] {
+        const lines = source.split(/\r?\n/);
+        const tokens: [string, number, number, string][] = [];
+
+        for (const [lineNumber, line] of lines.entries()) {
+            for (const [offset, literal] of this.lexLine(line)) {
+                tokens.push([fileName, lineNumber, offset, literal]);
+            }
+        }
+        
+        return tokens;
+    }
+
+    // Token transform
+
+    private parseToken(token: [string, number, number, string]): Token {
+        const literal = token[3];
+        switch (literal) {
+            case "+": {
+                return [TokenType.Plus, null];
+            }
+            case "-": {
+                return [TokenType.Minus, null];
+            }
+            default: {
+                return [TokenType.Int, literal];
+            }
+        }
+    }
+
     compile(filePath: string) {
-        Deno.readTextFileSync(filePath).split(/\r?\n/); // TODO
+        const source = Deno.readTextFileSync(filePath);
+        const tokens = this.lexFile(filePath, source);
+        const tokenStream = tokens.map(t => this.parseToken(t));
+
+        console.log(tokenStream);
     }
 }
 
 enum TokenType {
     Int,
-    Add,
+    Plus,
     Minus
 }
+
+type Token = [TokenType, string | null]
 
 class StackChain {
     public stackOffset: number = 0;
